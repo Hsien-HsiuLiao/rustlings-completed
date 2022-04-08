23ccacc7446b93628cc21cf457c6c6025c858f31
Hsien-Hsiu Liao <hsien-hsiu.liao@yandex.com>
2022-04-08 11:50:21 -0700

    completed move semantics

--- exercises/move_semantics/move_semantics5/src/lib.rs
+++ exercises/move_semantics/move_semantics5/src/lib.rs
@@ -1,9 +1,3 @@
-// move_semantics5.rs
-// Make me compile only by reordering the lines in `main()`, but without
-// adding, changing or removing any of them.
-// Execute `rustlings hint move_semantics5` for hints :)
-
-
 fn main() {
     let mut x = 100;
     let y = &mut x;
@@ -14,6 +8,6 @@ fn main() {
     println!("{}", *y == x);
 
     let z = &mut x;
-    *z += 1000;
+    z += 1000;
     assert_eq!(x, 1200);
 }
